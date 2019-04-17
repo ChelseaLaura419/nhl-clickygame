be4ddc218c1bccbf5aa3c1487a41306ce3b17fb0
Chelsea Laura <cdesrosiers419@gmail.com>
2019-04-16 21:18:27 -0400

    Updates

---
+++ precache-manifest.accb276c085bbbf545545d8eb938ca95.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1eb1addfad37848ade56a3d06384833",
+    "url": "/nhl-clickygame/static/media/lasvegas.e1eb1add.jpg"
+  },
+  {
+    "revision": "ac7e02951453b6795c01",
+    "url": "/nhl-clickygame/static/css/main.e06668c4.chunk.css"
+  },
+  {
+    "revision": "a0c603d1e2a304d6cfa5",
+    "url": "/nhl-clickygame/static/js/runtime~main.bbb28ea1.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/nhl-clickygame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "c1f1f201731a098fd885f7f3d1824961",
+    "url": "/nhl-clickygame/static/media/boston.c1f1f201.jpg"
+  },
+  {
+    "revision": "60eb94b00d448a0e199a7d83d1105a2d",
+    "url": "/nhl-clickygame/static/media/calgary.60eb94b0.jpg"
+  },
+  {
+    "revision": "417585a353b5c821591c8f54504d4e4e",
+    "url": "/nhl-clickygame/static/media/captials.417585a3.jpg"
+  },
+  {
+    "revision": "1dd1dbb6c8a60f865db8274827ce34d5",
+    "url": "/nhl-clickygame/static/media/carolina.1dd1dbb6.jpg"
+  },
+  {
+    "revision": "44f8a78e021c43226e6bf9b4b40efb22",
+    "url": "/nhl-clickygame/static/media/colorado.44f8a78e.jpg"
+  },
+  {
+    "revision": "ac7e02951453b6795c01",
+    "url": "/nhl-clickygame/static/js/main.101c9ea7.chunk.js"
+  },
+  {
+    "revision": "7d8a5c20f117a684b7d2f788b4b8a918",
+    "url": "/nhl-clickygame/static/media/nhlHeader.7d8a5c20.jpg"
+  },
+  {
+    "revision": "00cc4169be55c92c4396ce7365c5e577",
+    "url": "/nhl-clickygame/static/media/pitts.00cc4169.jpg"
+  },
+  {
+    "revision": "601a32505281ca128bd411fc6ebe64c8",
+    "url": "/nhl-clickygame/static/media/sanjose.601a3250.jpg"
+  },
+  {
+    "revision": "5a34cf80d3cd2900a5330c2c1e6df4e1",
+    "url": "/nhl-clickygame/static/media/stlouis.5a34cf80.jpg"
+  },
+  {
+    "revision": "5e4358234effb4725888bc56da054c2c",
+    "url": "/nhl-clickygame/static/media/tampa.5e435823.jpg"
+  },
+  {
+    "revision": "92760b71dac2e38bb2c5169ec077b286",
+    "url": "/nhl-clickygame/static/media/toronto.92760b71.jpg"
+  },
+  {
+    "revision": "828feb86a3bd5f3e0186cdea0215a95b",
+    "url": "/nhl-clickygame/static/media/winn.828feb86.jpg"
+  },
+  {
+    "revision": "b5a9435993df4b34b17467a2b1b20c67",
+    "url": "/nhl-clickygame/index.html"
+  }
+];
